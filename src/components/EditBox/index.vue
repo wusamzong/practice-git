1ebcae607a88ad6036d325f60416a8b4f79208a9
sam wu <t3742238@gmail.com>
2020-04-24 11:02:52 +0800

    Editbox中的版面完成

--- src/components/EditBox/index.vue
+++ src/components/EditBox/index.vue
@@ -4,6 +4,11 @@
 
 <script>
 export default {
+  data(){
+    return {
+      isMidi:false
+    }
+  },
   props:{
     EditWindows:{
       type: Number,
@@ -13,6 +18,55 @@ export default {
       type: Boolean,
       required: true
     }
-  }
+  },
 }
 </script>
+<style scope>
+.EditBox{
+  height: 100%;
+  display: grid;
+  grid-template-columns: 350px auto;
+  grid-template-rows: 50px auto;
+  grid-template-areas: 
+  "frame frame"
+  "adjust content";
+}
+.MidiEditor{
+  height: 100%;
+  display: grid;
+  grid-template-columns: 350px auto;
+  grid-template-rows: 50px auto;
+  grid-template-areas: 
+  "frame content"
+  "adjust content";
+}
+.frame{
+  grid-area: frame;
+  margin: 0px 20px;
+  border-bottom:#a8a8a8 0.5px solid;
+  display: flex;
+  align-items: center;
+
+}
+.frame button{
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  background: #858585;
+  border: 1px solid #858585;
+}
+.frame p{
+  color: white;
+  margin: 0 20px;
+  font-size: 25px;
+}
+.content{
+  grid-area: content;
+  background: #4b4b4b;
+}
+.adjust{
+  grid-area: adjust;
+  background: #353535;
+}
+
+</style>
